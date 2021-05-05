29c9b29e93afe1c533329b943b1b3e23cc6130ed
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-05 11:30:07 +0900

    [add] SearchInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecoundaryButton } from "./components/atoms/button/SecoundaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +8,8 @@ export default function App() {
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecoundaryButton>テスト</SecoundaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
