d96f4cefb31f8c38e64a6a81731210ae6a66877e
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-05 12:18:02 +0900

    [add ]UserCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecoundaryButton } from "./components/atoms/button/SecoundaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
 import "./styles.css";
+
+const user = {
+  name: "はち",
+  image: "https://source.unsplash.com/mEdKuPYJe1I",
+  email: "1234@example.com",
+  phone: "070-1111-1111",
+  company: {
+    name: "おはっけい株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +23,7 @@ export default function App() {
       <SecoundaryButton>テスト</SecoundaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
