d05b21ea403f18cc38492f70a03373ffac3ec4cc
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-05 13:51:49 +0900

    [add] User List

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecoundaryButton } from "./components/atoms/button/SecoundaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
 import { Router } from "./router/Router";
-
 import "./styles.css";
 
-const user = {
-  name: "はち",
-  image: "https://source.unsplash.com/mEdKuPYJe1I",
-  email: "1234@example.com",
-  phone: "0X0-1111-1111",
-  company: {
-    name: "おはっけい株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <Router />
-    // <DefaultLayout>
-    //   <PrimaryButton>テスト</PrimaryButton>
-    //   <SecoundaryButton>テスト</SecoundaryButton>
-    //   <br />
-    //   <SearchInput />
-    //   <UserCard user={user} />
-    // </DefaultLayout>
-  );
+  return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,46 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: "はち",
+    image: "https://source.unsplash.com/mEdKuPYJe1I",
+    email: "1234@example.com",
+    phone: "0X0-1111-1111",
+    company: {
+      name: "おはっけい株式会社"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
